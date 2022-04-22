6cc1d312707231362d14d8f6949e052d1529dc2b
nexteditorjs <nexteditorjs@gmail.com>
2022-04-22 17:17:59 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import {
+  assert,
+  createEditor,
+  createEmptyDoc,
+  getLogger,
+  LocalDoc,
+} from '@nexteditorjs/nexteditor-core';
+import { MarkdownInputHandler } from '@nexteditorjs/nexteditor-input-handlers';
+
+import './style.css';
+
+const logger = getLogger('main');
+
+const app = document.querySelector<HTMLDivElement>('#app');
+assert(logger, app, 'app does not exists');
+
+const editor = createEditor(app, new LocalDoc(createEmptyDoc() as any), {
+  components: {
+    // blocks: [ListBlock],
+  },
+});
+
+editor.input.addHandler(new MarkdownInputHandler());
+
+(window as any).editor = editor;
